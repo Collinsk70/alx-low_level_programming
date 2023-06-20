5f1eac766470d41c9304b513d37d139895817ec0
Collins Karweru <collinskarweru@gmail.com>
2023-06-20 15:57:52 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,26 +13,22 @@ void times_table(void)
 		{
 			product = i * j;
 
-			if (product < 10)
+			if (j != 0)
 			{
-				if (j != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-				_putchar(product + '0');
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-			}
-
-			if (j != 9)
 				_putchar(',');
+				_putchar(' ');
+			}
 
-			_putchar(' ');
+			if (product < 10 && j != 0)
+				_putchar(' ');
+
+			if (product < 10)
+				_putchar(' ');
+
+			if (product >= 10)
+				_putchar((product / 10) + '0');
+
+			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
 	}
