235d4ebe906f030cce775f1432d5c505f1b62643
Collins Karweru <collinskarweru@gmail.com>
2023-06-20 14:05:25 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,35 @@
+#include "main.h"
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    printf("_putchar\n");
-    return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to be written
+ *
+ * Return: On success, return the character written.
+ * On error, return EOF.
+ */
+int _putchar(char c)
+{
+	return (putchar(c));
+}
+
