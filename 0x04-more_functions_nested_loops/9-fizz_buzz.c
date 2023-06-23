75e21305b54cc316622e540339647b684cecdf1a
Collins Karweru <collinskarweru@gmail.com>
2023-06-22 18:21:05 -0700

    comit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * fizz_buzz - Prints numbers from 1 to 100 with FizzBuzz rules
@@ -10,16 +11,13 @@ void fizz_buzz(void)
     for (i = 1; i <= 100; i++)
     {
         if (i % 3 == 0 && i % 5 == 0)
-            printf("FizzBuzz");
+            printf("FizzBuzz ");
         else if (i % 3 == 0)
-            printf("Fizz");
+            printf("Fizz ");
         else if (i % 5 == 0)
-            printf("Buzz");
+            printf("Buzz ");
         else
-            printf("%d", i);
-
-        if (i != 100)
-            printf(" ");
+            printf("%d ", i);
     }
 
     printf("\n");
