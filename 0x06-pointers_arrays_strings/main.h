0114186902e1f2913e4438910fdb6fa4fc2d90c4
Collins Karweru <collinskarweru@gmail.com>
2023-06-29 14:16:24 -0700

    commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,4 +4,5 @@
 char *_strcat(char *dest, const char *src);
 char *_strncat(char *dest, const char *src, int n);
 char *_strncpy(char *dest, const char *src, int n);
+int _strcmp(const char *s1, const char *s2);
 #endif /* MAIN_H */
