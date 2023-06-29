5ed13f8feac45e46b67408ace5c47dd4554febc9
Collins Karweru <collinskarweru@gmail.com>
2023-06-29 14:24:10 -0700

    commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ char *_strncat(char *dest, const char *src, int n);
 char *_strncpy(char *dest, const char *src, int n);
 int _strcmp(const char *s1, const char *s2);
 void reverse_array(int *a, int n);
+char *string_toupper(char *str);
 
 #endif /* MAIN_H */
