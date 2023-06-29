8d787ab88ee320aa832943dc0949fa5c1878572c
Collins Karweru <collinskarweru@gmail.com>
2023-06-29 15:16:05 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13 cipher
+ * @str: Pointer to the string
+ *
+ * Return: Pointer to the modified string
+ */
+char *rot13(char *str)
+{
+	char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; alpha[j]; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = rot13[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
