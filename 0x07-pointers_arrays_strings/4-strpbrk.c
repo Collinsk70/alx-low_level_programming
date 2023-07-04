188117f6f799659b8ffa62934f85eacc0fa6a951
Collins Karweru <collinskarweru@gmail.com>
2023-07-04 12:57:00 -0700

    commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,18 +11,18 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-    char *s_ptr;
-    char *a_ptr;
+	char *s_ptr;
+	char *a_ptr;
 
-    for (s_ptr = s; *s_ptr != '\0'; s_ptr++)
-    {
-        for (a_ptr = accept; *a_ptr != '\0'; a_ptr++)
-        {
-            if (*s_ptr == *a_ptr)
-                return (s_ptr);
-        }
-    }
+	for (s_ptr = s; *s_ptr != '\0'; s_ptr++)
+	{
+		for (a_ptr = accept; *a_ptr != '\0'; a_ptr++)
+		{
+			if (*s_ptr == *a_ptr)
+				return (s_ptr);
+		}
+	}
 
-    return (NULL);
+	return (NULL);
 }
 
