cb6a326da69b1e19a720eb6b3948ab76edfefd6e
Collins Karweru <collinskarweru@gmail.com>
2023-07-05 15:24:06 -0700

    commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,13 +10,13 @@
  */
 int is_palindrome_recursive(char *s, int start, int end)
 {
-	if (start >= end)
-		return 1;
+    if (start >= end)
+        return 1;
 
-	if (s[start] != s[end])
-		return 0;
+    if (s[start] != s[end])
+        return 0;
 
-	return is_palindrome_recursive(s, start + 1, end - 1);
+    return is_palindrome_recursive(s, start + 1, end - 1);
 }
 
 /**
@@ -27,12 +27,11 @@ int is_palindrome_recursive(char *s, int start, int end)
  */
 int is_palindrome(char *s)
 {
-	int len = 0;
-	int i;
+    int len = 0;
 
-	while (s[len] != '\0')
-		len++;
+    while (s[len] != '\0')
+        len++;
 
-	return is_palindrome_recursive(s, 0, len - 1);
+    return is_palindrome_recursive(s, 0, len - 1);
 }
 
