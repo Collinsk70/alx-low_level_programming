6b1fb782afe5bd1b6b3b724d8d25b8a08db1fe96
Collins Karweru <collinskarweru@gmail.com>
2023-07-05 15:11:11 -0700

    comit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,6 +8,8 @@
  */
 int is_prime_number(int n)
 {
+    int i;
+
     if (n <= 1)
         return 0;
     if (n <= 3)
@@ -16,7 +18,6 @@ int is_prime_number(int n)
     if (n % 2 == 0 || n % 3 == 0)
         return 0;
 
-    int i;
     for (i = 5; i * i <= n; i += 6)
     {
         if (n % i == 0 || n % (i + 2) == 0)
