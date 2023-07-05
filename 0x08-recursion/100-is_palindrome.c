7be4aadaf9cb67d0a9eacce95c1b6f30142d39c0
Collins Karweru <collinskarweru@gmail.com>
2023-07-05 15:29:12 -0700

    commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * _strlen - Returns the length of a string
+ * @s: The string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen(s + 1));
+}
+
+/**
  * is_palindrome_recursive - Recursive helper function to check if a string is a palindrome
  * @s: The string to check
  * @start: The starting index
@@ -10,13 +24,13 @@
  */
 int is_palindrome_recursive(char *s, int start, int end)
 {
-    if (start >= end)
-        return 1;
+	if (start >= end)
+		return (1);
 
-    if (s[start] != s[end])
-        return 0;
+	if (s[start] != s[end])
+		return (0);
 
-    return is_palindrome_recursive(s, start + 1, end - 1);
+	return (is_palindrome_recursive(s, start + 1, end - 1));
 }
 
 /**
@@ -27,11 +41,7 @@ int is_palindrome_recursive(char *s, int start, int end)
  */
 int is_palindrome(char *s)
 {
-    int len = 0;
+	int len = _strlen(s);
 
-    while (s[len] != '\0')
-        len++;
-
-    return is_palindrome_recursive(s, 0, len - 1);
+	return (is_palindrome_recursive(s, 0, len - 1));
 }
-
