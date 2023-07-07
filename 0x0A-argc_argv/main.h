451885d01db629e56fc76fab77949f874ba38251
Collins Karweru <collinskarweru@gmail.com>
2023-07-07 16:19:05 -0700

    update

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -8,6 +8,7 @@
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
+int main(int argc, char *argv[])
 
 #endif /* MAIN_H */
 
