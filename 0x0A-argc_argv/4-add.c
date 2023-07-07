5475e393f1ae7a7ebf0cc1c9f4b2dae1ddcb8951
Collins Karweru <collinskarweru@gmail.com>
2023-07-07 16:58:06 -0700

    comi

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - adds positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
+ * is_positive_number - Checks if a string represents a positive number
+ * @str: The string to check
  *
- * Return: 0 if successful, 1 if error
+ * Return: 1 if the string is a positive number, 0 otherwise
+ */
+int is_positive_number(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+
+	return (1);
+}
+
+/**
+ * main - Adds positive numbers
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success, 1 if an error occurs
  */
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
-	
+
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	
+
 	for (i = 1; i < argc; i++)
 	{
 		if (is_positive_number(argv[i]))
@@ -32,25 +51,5 @@ int main(int argc, char *argv[])
 
 	printf("%d\n", sum);
 	return (0);
-}
-
-/**
- * is_positive_number - checks if a string is a positive number
- * @str: string to check
- *
- * Return: 1 if positive number, 0 otherwise
- */
-int is_positive_number(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		if (str[i] < '0' || str[i] > '9')
-			return (0);
-		i++;
-	}
-
-	return (1);
 }
 
