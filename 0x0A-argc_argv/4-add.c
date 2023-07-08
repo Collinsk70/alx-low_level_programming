8a0126b0d82c9743f7c3b6f90e987ed9b82ecdcd
Collins Karweru <collinskarweru@gmail.com>
2023-07-07 17:11:41 -0700

    comit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,30 +11,27 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
-	
-	if (argc == 1)
-	{
-		printf("0\n");
+    int i, sum = 0;
 
-		return (0);
-	}
-	
-	for (i = 1; i < argc; i++)
-	{
-		if (is_positive_number(argv[i]))
-			sum += atoi(argv[i]);
-		else
-		{
-			printf("Error\n");
+    if (argc == 1)
+    {
+        printf("0\n");
+        return (0);
+    }
 
-			return (1);
-		}
-	}
+    for (i = 1; i < argc; i++)
+    {
+        if (is_positive_number(argv[i]))
+            sum += atoi(argv[i]);
+        else
+        {
+            printf("Error\n");
+            return (1);
+        }
+    }
 
-	printf("%d\n", sum);
-
-	return (0);
+    printf("%d\n", sum);
+    return (0);
 }
 
 /**
@@ -45,16 +42,14 @@ int main(int argc, char *argv[])
  */
 int is_positive_number(char *str)
 {
-	int i = 0;
+    int i = 0;
 
-	while (str[i] != '\0')
-	{
-		if (str[i] < '0' || str[i] > '9')
+    while (str[i] != '\0')
+    {
+        if (str[i] < '0' || str[i] > '9')
+            return (0);
+        i++;
+    }
 
-			return (0);
-		i++;
-	}
-
-	return (1);
+    return (1);
 }
-
