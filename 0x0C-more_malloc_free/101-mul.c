948d8a3cad828f7fe8802564b78cc47efb2688c0
Collins Karweru <collinskarweru@gmail.com>
2023-07-14 18:12:27 -0700

    commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,15 +3,166 @@
 #include <ctype.h>
 #include "main.h"
 
-/* Function Declarations */
-int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
-void addCarry(char *final_prod, char *next_prod, int next_len);
-void addRemaining(char *final_prod, char *next_prod, int next_len);
-void addCarryAndRemaining(char *final_prod, char *next_prod, int next_len);
+/**
+ * find_len - Finds the length of a string.
+ * @str: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int find_len(char *str)
+{
+    int len = 0;
+
+    while (*str++)
+        len++;
+
+    return (len);
+}
+
+/**
+ * create_xarray - Creates an array of 'x' characters and adds a null byte.
+ * @size: The size of the array.
+ *
+ * Return: A pointer to the created array.
+ */
+char *create_xarray(int size)
+{
+    char *array;
+    int index;
+
+    array = malloc(sizeof(char) * size);
+
+    if (array == NULL)
+        exit(98);
+
+    for (index = 0; index < (size - 1); index++)
+        array[index] = 'x';
+
+    array[index] = '\0';
+
+    return (array);
+}
+
+/**
+ * iterate_zeroes - Iterates through a string to remove leading zeroes.
+ * @str: The string to iterate through.
+ *
+ * Return: A pointer to the next non-zero element.
+ */
+char *iterate_zeroes(char *str)
+{
+    while (*str && *str == '0')
+        str++;
+
+    return (str);
+}
+
+/**
+ * get_digit - Converts a character digit to an integer.
+ * @c: The character digit to be converted.
+ *
+ * Return: The converted integer digit.
+ */
+int get_digit(char c)
+{
+    int digit = c - '0';
+
+    if (digit < 0 || digit > 9)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    return (digit);
+}
+
+/**
+ * get_prod - Calculates the product of a number and a digit.
+ * @prod: The buffer to store the resulting product.
+ * @mult: The number as a string.
+ * @digit: The single digit to multiply with.
+ * @zeroes: The number of leading zeroes to append.
+ */
+void get_prod(char *prod, char *mult, int digit, int zeroes)
+{
+    int mult_len, num, tens = 0;
+
+    mult_len = find_len(mult) - 1;
+    mult += mult_len;
+
+    while (*prod)
+    {
+        *prod = 'x';
+        prod++;
+    }
+
+    prod--;
+
+    while (zeroes--)
+    {
+        *prod = '0';
+        prod--;
+    }
+
+    for (; mult_len >= 0; mult_len--, mult--, prod--)
+    {
+        if (*mult < '0' || *mult > '9')
+        {
+            printf("Error\n");
+            exit(98);
+        }
+
+        num = (*mult - '0') * digit;
+        num += tens;
+        *prod = (num % 10) + '0';
+        tens = num / 10;
+    }
+
+    if (tens)
+        *prod = (tens % 10) + '0';
+}
+
+/**
+ * add_nums - Adds two numbers represented as strings.
+ * @final_prod: The buffer to store the final product.
+ * @next_prod: The next product to add.
+ * @next_len: The length of next_prod.
+ */
+void add_nums(char *final_prod, char *next_prod, int next_len)
+{
+    int num, tens = 0;
+
+    while (*(final_prod + 1))
+        final_prod++;
+
+    while (*(next_prod + 1))
+        next_prod++;
+
+    for (; *final_prod != 'x'; final_prod--)
+    {
+        num = (*final_prod - '0') + (*next_prod - '0');
+        num += tens;
+        *final_prod = (num % 10) + '0';
+        tens = num / 10;
+
+        next_prod--;
+        next_len--;
+    }
+
+    for (; next_len >= 0 && *next_prod != 'x'; next_len--)
+    {
+        num = (*next_prod - '0');
+        num += tens;
+        *final_prod = (num % 10) + '0';
+        tens = num / 10;
+
+        final_prod--;
+        next_prod--;
+    }
+
+    if (tens)
+        *final_prod = (tens % 10) + '0';
+}
 
 /**
  * main - Entry point of the program.
@@ -72,195 +223,5 @@ int main(int argc, char *argv[])
     free(final_prod);
 
     return (0);
-}
-
-/**
- * find_len - Finds the length of a string.
- * @str: The string to be measured.
- *
- * Return: The length of the string.
- */
-int find_len(char *str)
-{
-    int len = 0;
-
-    while (*str++)
-        len++;
-
-    return (len);
-}
-
-/**
- * create_xarray - Creates an array of 'x' characters and adds
- * @size: The size of the array to be created.
- *
- * Return: A pointer to the created array.
- */
-char *create_xarray(int size)
-{
-    char *array;
-    int index;
-
-    array = malloc(sizeof(char) * size);
-
-    if (array == NULL)
-        exit(98);
-
-    for (index = 0; index < (size - 1); index++)
-        array[index] = 'x';
-
-    array[index] = '\0';
-
-    return (array);
-}
-
-/**
- * iterate_zeroes - Iterates through leading zeroes in a string.
- * @str: The string to be iterated.
- *
- * Return: A pointer to the first non-zero character.
- */
-char *iterate_zeroes(char *str)
-{
-    while (*str && *str == '0')
-        str++;
-
-    return (str);
-}
-
-/**
- * get_digit - Converts a character digit to an integer.
- * @c: The character digit to be converted.
- *
- * Return: The converted integer digit.
- */
-int get_digit(char c)
-{
-    int digit = c - '0';
-
-    if (digit < 0 || digit > 9)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-
-    return (digit);
-}
-
-/**
- * get_prod - Calculates the product of a number and a digit.
- * @prod: The buffer to store the result.
- * @mult: The number as a string.
- * @digit: The digit to multiply with.
- * @zeroes: The number of leading zeroes.
- */
-void get_prod(char *prod, char *mult, int digit, int zeroes)
-{
-    int mult_len, num, tens = 0;
-
-    mult_len = find_len(mult) - 1;
-    mult += mult_len;
-
-    while (*prod)
-    {
-        *prod = 'x';
-        prod++;
-    }
-
-    prod--;
-
-    while (zeroes--)
-    {
-        *prod = '0';
-        prod--;
-    }
-
-    for (; mult_len >= 0; mult_len--, mult--, prod--)
-    {
-        if (*mult < '0' || *mult > '9')
-        {
-            printf("Error\n");
-            exit(98);
-        }
-
-        num = (*mult - '0') * digit;
-        num += tens;
-        *prod = (num % 10) + '0';
-        tens = num / 10;
-    }
-
-    if (tens)
-        *prod = (tens % 10) + '0';
-}
-
-/**
- * add_nums - Adds two numbers stored as strings.
- * @final_prod: The buffer storing the final product.
- * @next_prod: The next product to be added.
- * @next_len: The length of next_prod.
- */
-void add_nums(char *final_prod, char *next_prod, int next_len)
-{
-    addCarry(final_prod, next_prod, next_len);
-    addRemaining(final_prod, next_prod, next_len);
-    addCarryAndRemaining(final_prod, next_prod, next_len);
-}
-
-/**
- * addCarry - Adds carry digits to the final product.
- * @final_prod: The buffer storing the final product.
- * @next_prod: The next product to be added.
- * @next_len: The length of next_prod.
- */
-void addCarry(char *final_prod, char *next_prod, int next_len)
-{
-    int num, tens = 0;
-
-    for (; *final_prod != 'x'; final_prod--)
-    {
-        num = (*final_prod - '0') + (*next_prod - '0');
-        num += tens;
-        *final_prod = (num % 10) + '0';
-        tens = num / 10;
-
-        next_prod--;
-        next_len--;
-    }
-}
-
-/**
- * addRemaining - Adds remaining digits to the final product.
- * @final_prod: The buffer storing the final product.
- * @next_prod: The next product to be added.
- * @next_len: The length of next_prod.
- */
-void addRemaining(char *final_prod, char *next_prod, int next_len)
-{
-    int num, tens = 0;
-
-    for (; next_len >= 0 && *next_prod != 'x'; next_len--)
-    {
-        num = (*next_prod - '0');
-        num += tens;
-        *final_prod = (num % 10) + '0';
-        tens = num / 10;
-
-        final_prod--;
-        next_prod--;
-    }
-}
-
-/**
- * addCarryAndRemaining - Adds carry and remaining digits
- * @final_prod: The buffer storing the final product.
- * @next_prod: The next product to be added.
- * @next_len: The length of next_prod.
- */
-void addCarryAndRemaining(char *final_prod, char *next_prod, int next_len)
-{
-    int tens = 0;
-
-    if (tens)
-        *final_prod = (tens % 10) + '0';
 }
 
