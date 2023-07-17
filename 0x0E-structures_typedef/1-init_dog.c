7a1852487f7d00eb4cd5db513b8ff41c8b27991e
Collins Karweru <collinskarweru@gmail.com>
2023-07-17 14:57:39 -0700

    commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <stdlib.h>
+#include <stdlib.>
 
 /**
  * init_dog - Initializes a variable of type struct dog.
@@ -12,11 +12,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
+	if (d == NULL)
+		return;
 
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
