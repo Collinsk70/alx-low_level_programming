59125323797eb670f8960322efe666df4bf41e52
Collins Karweru <collinskarweru@gmail.com>
2023-07-17 14:11:11 -0700

    commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents information about a dog.
+ * @name: The name of the dog (string).
+ * @age: The age of the dog (float).
+ * @owner: The owner of the dog (string).
+ */
+typedef struct dog {
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
+
+#endif /* DOG_H */
+
