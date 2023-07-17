91372efb801a9067e8dcf02c574f97459f48b566
Collins Karweru <collinskarweru@gmail.com>
2023-07-17 16:11:58 -0700

    commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,46 +7,46 @@
  * new_dog - Creates a new dog
  * @name: Pointer to dog's name
  * @age: Age of the dog
- * @owner: Poininter to owner
+ * @owner: Pointer to owner
  *
  * Return: Pointer to created dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *newDog;
-    char *nameCopy, *ownerCopy;
-    int nameLen, ownerLen;
+	dog_t *newDog;
+	char *nameCopy, *ownerCopy;
+	int nameLen, ownerLen;
 
-    /* Calculate the lengths of name and owner */
-    nameLen = strlen(name);
-    ownerLen = strlen(owner);
+	/* Calculate the lengths of name and owner */
+	nameLen = strlen(name);
+	ownerLen = strlen(owner);
 
-    /* Allocate memory for the new dog structure */
-    newDog = malloc(sizeof(dog_t));
-    if (newDog == NULL)
-        return (NULL);
+	/* Allocate memory for the new dog structure */
+	newDog = malloc(sizeof(dog_t));
+	if (newDog == NULL)
+		return (NULL);
 
-    /* Allocate memory for the copies of name and owner */
-    nameCopy = malloc((nameLen + 1) * sizeof(char));
-    ownerCopy = malloc((ownerLen + 1) * sizeof(char));
+	/* Allocate memory for the copies of name and owner */
+	nameCopy = malloc((nameLen + 1) * sizeof(char));
+	ownerCopy = malloc((ownerLen + 1) * sizeof(char));
 
-    if (nameCopy == NULL || ownerCopy == NULL)
-    {
-        free(newDog);
-        free(nameCopy);
-        free(ownerCopy);
-        return (NULL);
-    }
+	if (nameCopy == NULL || ownerCopy == NULL)
+	{
+		free(newDog);
+		free(nameCopy);
+		free(ownerCopy);
+		return (NULL);
+	}
 
-    /* Copy name and owner to the allocated memory */
-    strcpy(nameCopy, name);
-    strcpy(ownerCopy, owner);
+	/* Copy name and owner to the allocated memory */
+	strcpy(nameCopy, name);
+	strcpy(ownerCopy, owner);
 
-    /* Set the new dog's attributes */
-    newDog->name = nameCopy;
-    newDog->age = age;
-    newDog->owner = ownerCopy;
+	/* Set the new dog's attributes */
+	newDog->name = nameCopy;
+	newDog->age = age;
+	newDog->owner = ownerCopy;
 
-    return (newDog);
+	return (newDog);
 }
 
