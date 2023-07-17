53d85655c7406a1ea262096570e14d706887d954
Collins Karweru <collinskarweru@gmail.com>
2023-07-17 15:52:02 -0700

    commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,28 +14,37 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDog;
+	char *cpyName, *cpyOwner;
+	int lenName = 0, lenOwner = 0, i;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	while (name[lenName])
+		lenName++;
+	while (owner[lenOwner])
+		lenOwner++;
 
 	newDog = malloc(sizeof(dog_t));
 	if (newDog == NULL)
 		return (NULL);
 
-	newDog->name = strdup(name);
-	if (newDog->name == NULL)
-	{
-		free(newDog);
+	cpyName = malloc(lenName + 1);
+	if (cpyName == NULL)
 		return (NULL);
-	}
+	for (i = 0; name[i]; i++)
+		cpyName[i] = name[i];
+	cpyName[i] = '\0';
 
+	cpyOwner = malloc(lenOwner + 1);
+	if (cpyOwner == NULL)
+		return (NULL);
+	for (i = 0; owner[i]; i++)
+		cpyOwner[i] = owner[i];
+	cpyOwner[i] = '\0';
+
+	newDog->name = cpyName;
 	newDog->age = age;
-
-	newDog->owner = strdup(owner);
-	if (newDog->owner == NULL)
-	{
-		free(newDog->name);
-		free(newDog);
-		return (NULL);
-	}
-
+	newDog->owner = cpyOwner;
 	return (newDog);
 }
-
