5c9d6f278237af394261da9c7fe23ccb9682280c
Collins Karweru <collinskarweru@gmail.com>
2023-07-19 14:08:40 -0700

    commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,15 +19,15 @@ int (*get_op_func(char *s))(int, int)
         {"%", op_mod},
         {NULL, NULL}
     };
-    int i = 0;
+int i;
 
-    while (ops[i].op)
-    {
-        if (*(ops[i].op) == *s)
-            return (ops[i].f);
-        i++;
-    }
-
-    return (NULL);
+	i = 0;
+	while (i < 5)
+	{
+		if (ops[i].op[0] == s[0])
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
 
