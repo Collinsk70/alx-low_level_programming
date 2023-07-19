92fed4cdfac2d78e3638c977337fda64a530affd
Collins Karweru <collinskarweru@gmail.com>
2023-07-19 14:17:38 -0700

    commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,46 @@
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
 
 /**
- * main - main function
- * @argc: argument count
- * @argv: string of arguments in array
- * Return: 0
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: Always 0.
  */
-
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int (*o)(int, int);
+    int num1, num2;
+    int (*op_func)(int, int);
+    int result;
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
+    if (argc != 4)
+    {
+        printf("Error\n");
+        return (98);
+    }
 
-	o = get_op_func(argv[2]);
-	if (o == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+    op_func = get_op_func(argv[2]);
 
-	printf("%d\n", o(a, b));
+    if (op_func == NULL)
+    {
+        printf("Error\n");
+        return (99);
+    }
 
-	return (0);
+    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        return (100);
+    }
+
+    result = op_func(num1, num2);
+    printf("%d\n", result);
+
+    return (0);
 }
+
