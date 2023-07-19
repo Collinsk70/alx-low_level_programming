0cab6148a6e97accacc1bc90d9e41d1a30f9c960
Collins Karweru <collinskarweru@gmail.com>
2023-07-19 13:03:01 -0700

    commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stdio>
-#include <stdlib>
-
  /* Function prototype for the function to print a name using the function pointer */
 void print_name(char *name, void (*f)(char *));
 
