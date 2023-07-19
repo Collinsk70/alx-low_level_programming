ed010c6face08c5255b282405b713e65787b3f5f
Collins Karweru <collinskarweru@gmail.com>
2023-07-19 14:36:15 -0700

    commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,36 +11,36 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2;
-    int (*op_func)(int, int);
-    int result;
+	int num1, num2;
+	int (*op_func)(int, int);
+	int result;
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    op_func = get_op_func(argv[2]);
+	op_func = get_op_func(argv[2]);
 
-    if (op_func == NULL)
-    {
-        printf("Error\n");
-        return (99);
-    }
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
 
-    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        return (100);
-    }
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
 
-    result = op_func(num1, num2);
-    printf("%d\n", result);
+	result = op_func(num1, num2);
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
 
