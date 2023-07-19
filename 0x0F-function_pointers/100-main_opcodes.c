b2bb278cc16f4031b4ad544f9fb61ae5c6b0cd83
Collins Karweru <collinskarweru@gmail.com>
2023-07-19 16:00:33 -0700

    commit

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,43 +5,34 @@
  * print_main_opcodes - Prints the opcodes of the main function.
  * @num_bytes: The number of bytes to print from the main function.
  */
-void print_main_opcodes(int num_bytes)
+int main(int a, char *b[])
 {
-    if (num_bytes <= 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
+	int bts, i;
 
-    // Create a function pointer to the main function
-    void (*main_function)(void) = main;
+	if (a != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-    // Loop through the opcodes and print them
-    for (int i = 0; i < num_bytes; i++)
-    {
-        printf("%02x ", ((unsigned char *)main_function)[i]);
-    }
-    printf("\n");
-}
+	bts = atoi(b[1]);
 
-/**
- * main - Entry point of the program.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the command-line arguments.
- *
- * Return: 0 on success, 1 if incorrect number of arguments provided, 2 if num_bytes is negative.
- */
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return 1;
-    }
+	if (bts < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-    int num_bytes = atoi(argv[1]);
-    print_main_opcodes(num_bytes);
+	/* Print the opcodes of the main function */
+	unsigned char *adrs = (unsigned char *)&main;
+	for (i = 0; i < bts; i++)
+	{
+		printf("%02x", adrs[i]);
+		if (i < bts - 1)
+			printf(" ");
+	}
+	printf("\n");
 
-    return 0;
+	return (0);
 }
 
