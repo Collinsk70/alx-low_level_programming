f88e21e38d3128c8c0a4408d85dae103588707f4
Collins Karweru <collinskarweru@gmail.com>
2023-07-19 14:00:35 -0700

    commit

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+    int num1, num2;
+    int (*op_func)(int, int);
+    int result;
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        return (98);
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+
+    op_func = get_op_func(argv[2]);
+
+    if (op_func == NULL)
+    {
+        printf("Error\n");
+        return (99);
+    }
+
+    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        return (100);
+    }
+
+    result = op_func(num1, num2);
+    printf("%d\n", result);
+
+    return (0);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Function to perform addition
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: The result of the addition operation.
+ */
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * op_sub - Function to perform subtraction
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: The result of the subtraction operation.
+ */
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * op_mul - Function to perform multiplication
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: The result of the multiplication operation.
+ */
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * op_div - Function to perform division
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: The result of the division operation.
+ */
+int op_div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * op_mod - Function to perform modulo
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: The result of the modulo operation.
+ */
+int op_mod(int a, int b)
+{
+    return (a % b);
+}
+
