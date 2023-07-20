ec96d9a3e5f9caa6f238c175a2fe5c43430eec59
Collins Karweru <collinskarweru@gmail.com>
2023-07-20 12:03:30 -0700

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,18 +10,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    if (n == 0)
-        return (0);
-
-    int sum = 0;
+    unsigned int i;
+    int sum;
     va_list args;
-    va_start(args, n);
 
-    for (unsigned int i = 0; i < n; ++i)
+    va_start(args, n);
+    for (i = 0, sum = 0; i < n; i++)
     {
         sum += va_arg(args, int);
     }
-
     va_end(args);
 
     return (sum);
