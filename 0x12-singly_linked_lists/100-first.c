7c46f456fb2b12c5c8b66f225dba915f634efed9
Collins Karweru <collinskarweru@gmail.com>
2023-07-29 10:56:42 -0700

    commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /* The constructor function to print the message */
-void before_main(void) __attribute__((constructor));
+void print_message(void) __attribute__((constructor));
 
 /**
- * before_main - Function executed before the main function.
+ * print_message - Function executed before the main function.
  *
  * Description: This function is automatically executed before the main
  *              function when the program starts, and it prints the message.
  */
-void before_main(void)
+void print_message(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 		   "I bore my house upon my back!\n");
@@ -22,9 +22,7 @@ void before_main(void)
  */
 int main(void)
 {
-	/* The main function */
-	printf("This is the main function.\n");
-
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 	return (0);
 }
 
