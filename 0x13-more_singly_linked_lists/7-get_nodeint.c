5c2b0260a51f9f5b09641c53ee0b713037b252ad
Collins Karweru <collinskarweru@gmail.com>
2023-08-01 08:08:18 -0700

    commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,11 +10,12 @@
 listint_t *get_nodeint_at_index(listint_t *head, 
 		unsigned int index)
 {
+
 	listint_t *current;
 
 	unsigned int count = 0;
 
-	/* Traverse the list to the desired index and of the list */
+	/* Traverse the list to the desired index and */
 	current = head;
 
 	while (current != NULL && count < index)
