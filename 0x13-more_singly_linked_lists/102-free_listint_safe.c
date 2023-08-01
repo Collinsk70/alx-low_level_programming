18ba7897dfa7dbe089a23dcbac3bf4548a33f4ee
Collins Karweru <collinskarweru@gmail.com>
2023-08-01 09:28:12 -0700

    commit

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,66 +2,37 @@
 #include <stdlib.h>
 
 /**
- * find_listint_loop - Detects if there is a loop in a linked list.
+ * free_listint_safe - Frees a listint_t list, even if it has a loop.
  *
- * @head: Pointer to the head of the linked list.
+ * @h: Double pointer to the head of the linked list.
  *
- * Return: Address of thop starts/returns, or NULL if no loop.
- */
-listint_t *find_listint_loop(listint_t *head)
-{
-	listint_t *current, *end;
-
-	if (head == NULL)
-		return (NULL);
-
-	for (end = head->next; end != NULL; end = end->next)
-	{
-		if (end == end->next)
-			return (end);
-		for (current = head; current != end; current = current->next)
-			if (current == end->next)
-				return (end->next);
-	}
-	return (NULL);
-}
-
-/**
- * free_listint_safe - Frees a listint list, even if it contains a loop.
- *
- * @h: Pointer to the head of the list.
- *
- * Return: Number of nodes that were freed.
+ * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *nxt, *loop_node;
-	size_t node_count;
-	int has_loop = 1;
+	size_t size = 0;
+	listint_t *current, *next;
 
 	if (h == NULL || *h == NULL)
 		return (0);
 
-	loop_node = find_listint_loop(*h);
-	for (node_count = 0; (*h != loop_node || has_loop) && *h != NULL; *h = nxt)
+	current = *h;
+
+	while (current != NULL)
 	{
-		node_count++;
-		nxt = (*h)->next;
-		if (*h == loop_node && has_loop)
+		size++;
+		next = current->next;
+		free(current);
+		current = next;
+
+		if (current == *h)
 		{
-			if (loop_node == loop_node->next)
-			{
-				free(*h);
-				break;
-			}
-			node_count++;
-			nxt = nxt->next;
-			free((*h)->next);
-			has_loop = 0;
+			/* Handle case of loop */
+			*h = NULL;
+			break;
 		}
-		free(*h);
 	}
-	*h = NULL;
-	return (node_count);
+
+	return (size);
 }
 
