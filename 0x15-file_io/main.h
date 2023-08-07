2c58ffd567c513e9cd933ac07f422c2b9beab3ee
Collins Karweru <collinskarweru@gmail.com>
2023-08-07 15:39:43 -0700

    commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,11 +8,15 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 int main(int argc, char *argv[]);
+void error_exit(const char *msg);
+void print_elf_header_info(Elf64_Ehdr *header);
+
 #endif /* MAIN_H */
 
