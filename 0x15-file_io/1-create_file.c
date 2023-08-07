d838bdcc9219a66bf67c0930ec010d53cb3d8091
Collins Karweru <collinskarweru@gmail.com>
2023-08-07 15:06:55 -0700

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,29 +11,31 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file_descriptor, bytes_written = 0;
+	int file_descriptor, bytes_written = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (file_descriptor == -1)
-        return (-1);
+	file_descriptor = open(filename,
+			       O_WRONLY | O_CREAT | O_TRUNC,
+			       S_IRUSR | S_IWUSR);
+	if (file_descriptor == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[bytes_written])
-            bytes_written++;
+	if (text_content != NULL)
+	{
+		while (text_content[bytes_written])
+			bytes_written++;
 
-        bytes_written = write(file_descriptor, text_content, bytes_written);
-        if (bytes_written == -1)
-        {
-            close(file_descriptor);
-            return (-1);
-        }
-    }
+		bytes_written = write(file_descriptor, text_content, bytes_written);
+		if (bytes_written == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
 
-    close(file_descriptor);
-    return (1);
+	close(file_descriptor);
+	return (1);
 }
 
