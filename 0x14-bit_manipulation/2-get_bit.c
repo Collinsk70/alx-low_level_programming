30be44132042ad051e8b4223d2f95844b9d1c932
Collinsk70 <collinskarweru@gmail.com>
2023-09-07 05:03:12 -0700

    commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,13 +5,13 @@
  * @n: The unsigned long int number.
  * @index: The index of the bit to get, starting from 0.
  *
- * Return: The value of the bit aif an error occurs.
+ * Return: The value of1 if an error occurs.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (index >= sizeof(unsigned long int) * 8)
-        return (-1);
+	if (index >= sizeof(unsigned long int) * 8) /* Check*/
+		return (-1);
 
-    return ((n >> index) & 1);
+	return ((n >> index) & 1); /* Right-t value */
 }
 
